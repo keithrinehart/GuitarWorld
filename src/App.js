4f58e81e300af8fb8285433ec4e2ef716509bc56
Keith Rinehart <rinehartkeith100@gmail.com>
2021-04-17 22:38:40 +0200

    router test6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import "./App.css";
 import { css } from "@emotion/react";
 import Home from "./pages/home";
@@ -8,7 +8,6 @@ import Epiphone from "./pages/epiphone";
 import Jackson from "./pages/jackson";
 import Ibanez from "./pages/ibanez";
 import Four from "./pages/four";
-import { render } from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./components/ScrollBehavior";
 import ScaleLoader from "react-spinners/ScaleLoader";
@@ -31,42 +30,43 @@ const loaderCSS = css`
   }
 `;
 
-const App = () => {
-  const [loading, setLoading] = useState(true);
+class App extends Component {
+  render() {
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 4000); //3sec.
-  }, []);
-  return (
-    <div className="App">
-      {loading ? (
-        <ScaleLoader
-          css={loaderCSS}
-          color={"rgb(240, 91, 22)"}
-          loading
-          size={30}
-        />
-      ) : (
-        <BrowserRouter>
-          <ScrollToTop>
-            <Switch>
-              <Route exact path="/GuitarWorld/" component={Home} />
-              <Route path="/gibson/" component={Gibson} />
-              <Route path="/fender/" component={Fender} />
-              <Route path="/epiphone/" component={Epiphone} />
-              <Route path="/jackson/" component={Jackson} />
-              <Route path="/ibanez/" component={Ibanez} />
-              <Route component={Four} />
-            </Switch>
-          </ScrollToTop>
-        </BrowserRouter>
-      )}
-    </div>
-  );
-};
+    useEffect(() => {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 4000); //3sec.
+    }, []);
+    return (
+      <div className="App">
+        {loading ? (
+          <ScaleLoader
+            css={loaderCSS}
+            color={"rgb(240, 91, 22)"}
+            loading
+            size={30}
+          />
+        ) : (
+          <BrowserRouter>
+            <ScrollToTop>
+              <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/gibson" component={Gibson} />
+                <Route path="/fender" component={Fender} />
+                <Route path="/epiphone" component={Epiphone} />
+                <Route path="/jackson" component={Jackson} />
+                <Route path="/ibanez" component={Ibanez} />
+                <Route component={Four} />
+              </Switch>
+            </ScrollToTop>
+          </BrowserRouter>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
-render(<App />, document.getElementById("root"));
